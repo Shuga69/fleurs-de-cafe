15f3edc2decec5b53c72f15a72f647b049383204
Shuga69 <54189758+Shuga69@users.noreply.github.com>
2022-02-11 18:59:15 +0200

    add pagination

--- coffee/coffee.js
+++ coffee/coffee.js
@@ -1,4 +1,5 @@
 let mockData=[
+    {id:0, price:"$15.50",image:"../assets/releaseImage3.jpg"},
     {id:1, price:"$15.50",image:"../assets/releaseImage3.jpg"},
     {id:2,price:"$15.50",image:"../assets/releaseImage3.jpg"},
     {id:3,price:"$15.50",image:"../assets/releaseImage3.jpg"},
@@ -32,53 +33,144 @@ let mockData=[
     {id:31,price:"$15.50",image:`../assets/releaseImage3.jpg`},
 ]
 
-let elements_per_page = 5;
+let elements_per_page = 6;
 let pages_count = Math.ceil(mockData.length/elements_per_page);
 
-function renderButton(pages_count){
-    let str='';
-    for (let i =1; i<=pages_count;++i)
-    str+=`<button class="pagination-group-button">${i}</button>`
+function renderButton(pages_count, mockData ){
+   let str='';
+   let current_page = 0
+  
+    for (let i =1; i<=pages_count;++i){
+        str+=`<button id="pagination-group-button" class="pagination-group-button">${i}</button>`   
+    }
     
+    let nextButton = `<button id="next-button" class="pagination-group-button-arrow"><img src="../assets/right-button-arrow.svg"/></button>`
     document.getElementById('pagination-group').innerHTML += str
+    document.getElementById('pagination-group').innerHTML += nextButton
+    let buttons = document.getElementsByClassName('pagination-group-button')
+    let meta = pagination(mockData.length-1,1,elements_per_page,pages_count);
+    renderDesktop(meta, mockData)
+    renderMobile(meta,mockData)
+    document.getElementById('next-button').addEventListener('click', ()=>{
+        console.log(meta.currentPage!==meta.totalPages)
+        if(meta.currentPage!==meta.totalPages){
+
+        meta = pagination(mockData.length,meta.nextPage,elements_per_page,pages_count)
+        renderDesktop(meta,mockData,true)
+        console.log("next "+meta.currentPage)
+        }
+        else meta=pagination(mockData.length,meta.currentPage,elements_per_page,pages_count); renderDesktop(meta,mockData,true);
+        }
+        ,false)
+  
+    for(let i =1;i<=pages_count;++i)
+    {
+        let meta = pagination(mockData.length,i,elements_per_page,pages_count)
+        buttons[current_page].addEventListener("click",()=>{renderDesktop(meta,mockData); console.log("num "+meta.currentPage); renderMobile(meta,mockData)} , false)
+        current_page++;
+        
+    }
 }
-function renderDesktop(price,image) {
+function renderDesktop(meta,mockData,next) {
     let str = '';
-    str = `<div class="hover-release-card desktop"><div style="background-image: url(${image})" class="newest-release__card__first">
+    let buttons = document.getElementsByClassName('pagination-group-button')
+    
+    let current = document.getElementsByClassName("active");
+  
+    current[1].className = current[1].className.replace("active", "");
+    buttons[meta.currentPage-1].classList.add('active')
+    
+    document.getElementById('card-container').innerHTML = str;
+    for(let i=meta.startIndex;i<meta.endIndex;i++){
+
+    str += `<div class="hover-release-card desktop"><div style="background-image: url(${mockData[i].image})" class="newest-release__card__first">
     <div class="hover-release-card-action">
         <img src="../assets/Heart_icon.svg"/>
         <div>
-            <p>${price}</p>
+            <p>${mockData[i].price}</p>
             <p>Dark city</p>    
         </div>
-       
         <p>ADD TO CART</p>
     </div>
 </div></div>`
+    }
 
     document.getElementById('card-container').innerHTML += str;
 }
-function renderMobile() {
+function renderMobile(meta,mockData) {
     let str = '';
-    str = `<div class="hover-release-card mobile">
-    <img width="165px" src="../assets/releaseImage2.jpg"/>
+    let buttons = document.getElementsByClassName('pagination-group-button')
+   
+    let current = document.getElementsByClassName("active");
+  
+    current[1].className = current[1].className.replace("active", "");
+    buttons[meta.currentPage-1].classList.add('active')
+
+    for(let i=meta.startIndex;i<meta.endIndex;i++){
+    str += `<div class="hover-release-card mobile">
+    <img width="165px" src="${mockData[i].image}"/>
     <div class="hover-release-card">
-       
         <div>
-            <p>$15.50</p>
-            
-            <p>Dark city</p>   
+            <p>${mockData[i].price}</p>
+            <p>Dark city</p>
             <p class="add-btn">ADD TO CART</p> 
         </div>
-       
-      
     </div>
     </div>`
-
+}
     document.getElementById('card-container').innerHTML += str;
 }
-renderButton(pages_count);
-for(let i = 0 ;i<15;++i){
-renderDesktop(mockData[i].price,mockData[i].image);
-renderMobile();
-}
+renderButton(pages_count,mockData);
+
+function pagination(
+    totalItems,
+    currentPage,
+    pageSize,
+    maxPages
+    ){
+        let totalPages = Math.ceil(totalItems / pageSize);
+        if (currentPage < 1) {
+            currentPage = 1;
+        } else if (currentPage > totalPages) {
+            currentPage = totalPages;
+        }
+        let startPage, endPage;
+        if (totalPages <= maxPages) {
+           
+            startPage = 1;
+            endPage = totalPages;
+        } else {
+            
+            let maxPagesBeforeCurrentPage = Math.floor(maxPages / 2);
+            let maxPagesAfterCurrentPage = Math.ceil(maxPages / 2) - 1;
+            if (currentPage <= maxPagesBeforeCurrentPage) {
+              
+                startPage = 1;
+                endPage = maxPages;
+            } else if (currentPage + maxPagesAfterCurrentPage >= totalPages) {
+               
+                startPage = totalPages - maxPages + 1;
+                endPage = totalPages;
+            } else {
+               
+                startPage = currentPage - maxPagesBeforeCurrentPage;
+                endPage = currentPage + maxPagesAfterCurrentPage;
+            }
+        }
+        let startIndex = (currentPage - 1) * pageSize;
+        let endIndex = Math.min(startIndex + pageSize, totalItems);
+        let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+        let nextPage = currentPage+1
+        return {
+            totalItems: totalItems,
+            currentPage: currentPage,
+            pageSize: pageSize,
+            totalPages: totalPages,
+            startPage: startPage,
+            endPage: endPage,
+            startIndex: startIndex,
+            endIndex: endIndex,
+            pages: pages,
+            nextPage: nextPage
+        };
+    }
